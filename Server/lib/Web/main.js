c4940dfae699feba5095bc66d23600d9fd8666db
pkquell <pkquell@gmail.com>
2017-02-16 17:12:38 +0900

    Update main.js

--- Server/lib/Web/main.js
+++ Server/lib/Web/main.js
@@ -211,11 +211,7 @@ Server.get("/", function(req, res){
 			'EN_INJEONG': Const.EN_INJEONG,
 			'KO_THEME': Const.KO_THEME,
 			'EN_THEME': Const.EN_THEME,
-			'IJP_EXCEPT': Const.IJP_EXCEPT,
-			'ogImage': "http://kkutu.kr/img/kkutu/logo.png",
-			'ogURL': "http://kkutu.kr/",
-			'ogTitle': "글자로 놀자! 끄투 온라인",
-			'ogDescription': "끝말잇기가 이렇게 박진감 넘치는 게임이었다니!"
+			'IJP_EXCEPT': Const.IJP_EXCEPT
 		});
 	}
 });
