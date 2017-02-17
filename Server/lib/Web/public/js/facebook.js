d6849462d9b98772f5895ec5f35deb4ff56905cc
pkquell <pkquell@gmail.com>
2017-02-17 19:13:16 +0900

    Create facebook.js

---
+++ Server/lib/Web/public/js/facebook.js
@@ -0,0 +1,10 @@
+window.fbAsyncInit = function() {
+	FB.init({appId: '?????????????', status: true, cookie: true,xfbml: true});
+};
+(function(d){
+	var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+	if (d.getElementById(id)) {return;}
+	js = d.createElement('script'); js.id = id; js.async = true;
+	js.src = "//connect.facebook.net/ko_KR/all.js";
+	ref.parentNode.insertBefore(js, ref);
+}(document));
