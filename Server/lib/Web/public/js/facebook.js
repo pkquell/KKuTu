62bd3d4a0e68912356da573865db6226de96370f
ljs970624 <ljs970624@gmail.com>
2017-03-08 06:02:58 +0900

    Update facebook.js

--- Server/lib/Web/public/js/facebook.js
+++ Server/lib/Web/public/js/facebook.js
@@ -1,10 +1,47 @@
+/**
+ * Rule the words! KKuTu Online
+ * Copyright (C) 2017 JJoriping(op@jjo.kr)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 window.fbAsyncInit = function() {
-	FB.init({appId: '?????????????', status: true, cookie: true,xfbml: true});
+	FB.init({
+		appId: '?????????????????',
+		xfbml: true,
+		version: 'v2.8'
+	});
+	/*
+	document.getElementById("ui-share-button").addEventListener("click", function() {
+		FB.ui({
+			method: 'share',
+			href: 'http://kkutu.io/',
+		},
+		function(response) {
+			if (response && !response.error_message) {
+				console.log('Posting completed.');
+			} else {
+				console.log('Error while posting.');
+			}
+		});
+	});
+	*/
 };
-(function(d){
-	var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+(function(d, s, id){
+	var js, fjs = d.getElementsByTagName(s)[0];
 	if (d.getElementById(id)) {return;}
-	js = d.createElement('script'); js.id = id; js.async = true;
-	js.src = "//connect.facebook.net/ko_KR/all.js";
-	ref.parentNode.insertBefore(js, ref);
-}(document));
+	js = d.createElement(s); js.id = id;
+	js.src = "//connect.facebook.net/ko_KR/sdk.js";
+	fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
