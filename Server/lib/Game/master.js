0043c0808883522c73b6fd3daecdbf2590bd2ac1
ljs970624 <ljs970624@gmail.com>
2017-03-12 05:30:04 +0900

    Update master.js

--- Server/lib/Game/master.js
+++ Server/lib/Game/master.js
@@ -316,7 +316,7 @@ exports.init = function(_SID, CHAN){
 					return;
 				}
 				if($c.guest){
-					if(SID != "0"){
+					if(SID != "0" && SID != "1"){
 						$c.sendError(402);
 						$c.socket.close();
 						return;
