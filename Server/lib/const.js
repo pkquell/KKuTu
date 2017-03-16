ed027b1eae426dda491493054d0bdc61a2e717e7
ljs970624 <ljs970624@gmail.com>
2017-03-17 06:42:36 +0900

    Update const.js

--- Server/lib/const.js
+++ Server/lib/const.js
@@ -199,7 +199,7 @@ exports.MISSION_en = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l
 
 exports.KO_INJEONG = [
 	"IMS", "VOC", "KRR", "KTV",
-	"NSK", "KOT", "DOT", "DRR", "DGM", "RAG", "LVL",
+	"NSK", "KOT", "DOT", "THP", "DRR", "DGM", "RAG", "LVL",
 	"LOL", "MRN", "MMM", "MAP", "MKK", "MNG",
 	"MOB", "HYK", "CYP", "HRH", "STA", "OIJ",
 	"KGR", "ESB", "ELW", "OIM", "OVW", "NEX", /*"WOW",*/
