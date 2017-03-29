f967073f2fd348ff26d794c68008f5a6c217650c
ljs970624 <ljs970624@gmail.com>
2017-03-30 08:34:44 +0900

    Update in_login.js

--- Server/lib/Web/lib/in_login.js
+++ Server/lib/Web/lib/in_login.js
@@ -18,98 +18,28 @@
 
 (function(){
 	$(document).ready(function(){
-		var fbStatus;
-		var fbTimer = setInterval(function(){
-			if(!FB) return;
-			try{
-				FB.getLoginStatus(function(res){
-					fbStatus = res;
-				});
-			}catch(e){
-				return;
-			}
-			clearInterval(fbTimer);
-		}, 400);
-		var ggStatus = false;
-		var ggTimer = setInterval(function(){
-			if(!gapi) return;
-			gapi.signin2.render('with-google', {
-				scope: 'profile email https://www.googleapis.com/auth/plus.login',
-				width: 210,
-				height: 35,
-				longtitle: true,
-				onsuccess: gOK,
-				onfailure: gNO
-			});
-			clearInterval(ggTimer);
-		}, 400);
 		$(".with-naver").on('click', function(e){
 			var url = "https://nid.naver.com/oauth2.0/authorize?";
-			
 			url += "response_type=code&";
-			url += "client_id=?????????????&";
-			url += "redirect_uri="+encodeURI("http://kkutu.io")+"&";
+			url += "client_id=★&";
 			url += "state="+encodeURI($("#stateKey").html())+"&";
-			
-			redirect(url);
+			url += "redirect_uri="+encodeURI("http://kkutu.io/?authType=naver");
+			location.href = url;
 		});
-		/*$(".with-twitter").on('click', function(e){
-			redirect("/login/twitter");
-		});*/
 		$(".with-facebook").on('click', function(e){
-			if(!FB){
-				alert("Please wait... (1)");
-				return;
-			}
-			if(!fbStatus){
-				FB.getLoginStatus(function(res){ fbStatus = res; });
-				alert("Please wait... (2)");
-				return;
-			}
-			
-			if(fbStatus.status == "connected"){
-				redirect("/?token="+fbStatus.authResponse.accessToken);
-			}else{
-				if ($.cookie("isapp") == "1") {
-					location.href = "https://www.facebook.com/dialog/oauth?client_id=????????????????&redirect_uri=" + encodeURI('http://kkutu.io/facebook_redirect.html') + "&scope=user_birthday&response_type=token";
-				} else {
-					FB.login(function(res){
-						if(res.authResponse){
-							redirect("/?token="+res.authResponse.accessToken);
-						}else{
-							history.back();
-						}
-					}, { scope: "user_birthday" });
-				}
-			}
+			var url = "https://www.facebook.com/dialog/oauth?";
+			url += "client_id=★&";
+			url += "redirect_uri="+encodeURI('http://kkutu.io/?authType=facebook')+"&";
+			url += "scope=user_birthday";
+			location.href = url;
 		});
-		if ($.cookie("isapp") == "1") {
-			$("#with-google").css("display", "none");
-		} else {
-			$("#with-google").on('click', function(e){
-				ggStatus = true;
-			});
-		}
-		function gOK(user){
-			if(!ggStatus) return;
-			var ar = user.getAuthResponse(true);
-			
-			console.log(ar);
-			$.post("/login/google", { it: ar.id_token, at: ar.access_token }, onLoggedIn);
-		}
-		function gNO(err){
-			if(!ggStatus) return;
-			alert(JSON.stringify(err));
-		}
-		function onLoggedIn(res){
-			if(res.error) return alert("ERROR " + res.error);
-			redirect("/register");
-		}
-		function redirect(to){
-			$.post("/session/set", { bm: $.cookie('bm') }, function(res){
-				$.cookie('prevpage', document.referrer);
-				location.href = to;
-			});
-		}
+		$(".with-google").on('click', function(e){
+			var url = "https://accounts.google.com/o/oauth2/auth?";
+			url += "client_id=★&";
+			url += "response_type=code&";
+			url += "scope=profile+email&";
+			url += "redirect_uri="+encodeURI('http://kkutu.io/?authType=google');
+			location.href = url;
+		});
 	});
 })();
